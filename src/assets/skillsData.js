967af8ae5fb7f5074ef76213ceef368f32bdd6fd
himanshsoni <himanshsoni5@gmail.com>
2021-08-31 12:00:53 +0530

    skills update

--- src/assets/skillsData.js
+++ src/assets/skillsData.js
@@ -22,5 +22,9 @@ export const skillList = [
     {
         skillName:'Python',
         skillUrl:'https://firebasestorage.googleapis.com/v0/b/portfolio-assets-580b7.appspot.com/o/python.png?alt=media&token=80ee3057-c54c-4246-8330-ca571b35dffa',
+    },
+    {
+        skillName:'Django',
+        skillUrl:'https://firebasestorage.googleapis.com/v0/b/portfolio-assets-580b7.appspot.com/o/django.png?alt=media&token=63f700b1-0f10-4c05-9155-8cba140605fe'
     }
 ];
